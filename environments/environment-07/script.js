693637bc8274b87adf76e2870e7d71eba35e596c
OliKirk <owk0907@gmail.com>
2023-05-31 11:11:39 +0200

    1

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+const songs = {
+  artist: "Taylor Swift",
+  title: "Blank space",
+  duration: "4:33",
+  artist: "Beastie Boys",
+  title: "Sabotage",
+  duration: "3:02",
+  artist: "Skrillex",
+  title: "Bangarang",
+  duration: "3:35",
+  artist: "Wolfgang Amadeus Mozart",
+  title: "Eine kleine Nachtmusik",
+  duration: "5:45",
+  artist: "Coldplay",
+  title: "Yellow",
+  duration: "4:27",
+  artist: "Metallica",
+  title: "Enter Sandman",
+  duration: "5:32",
+};
+
+function initApp() {
+  console.log(songs);
+  listOfSongs();
+}
+
+function listOfSongs(songs) {
+  for (const song of songs) {
+    console.log(songs);
+  }
+}
--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,56 @@
 "use strict";
+
+"use strict";
+
+window.addEventListener("load", initApp);
+
+let songs = [];
+
+function initApp() {}
+
+function name(artist, title, duration) {}
+
+function createSongClicked(event) {}
+
+function showSongs(listOfSongs) {}
+
+function sortBy(event) {}
+
+function sortSongs(value) {
+  let sortedSongs = [];
+  if (value === "artist") {
+  }
+}
+
+// const songs = {
+//   artist: "Taylor Swift",
+//   title: "Blank space",
+//   duration: "4:33",
+//   artist: "Beastie Boys",
+//   title: "Sabotage",
+//   duration: "3:02",
+//   artist: "Skrillex",
+//   title: "Bangarang",
+//   duration: "3:35",
+//   artist: "Wolfgang Amadeus Mozart",
+//   title: "Eine kleine Nachtmusik",
+//   duration: "5:45",
+//   artist: "Coldplay",
+//   title: "Yellow",
+//   duration: "4:27",
+//   artist: "Metallica",
+//   title: "Enter Sandman",
+//   duration: "5:32",
+// };
+
+// function initApp() {
+//   console.log(songs);
+//   listOfSongs();
+// }
+
+// function listOfSongs(songs) {
+//   for (const song of songs) {
+//     console.log(songs);
+//     let myHTML = /*HTML*/ `<li>${song.title} ${song.duration} </li>`;
+//   }
+// }
