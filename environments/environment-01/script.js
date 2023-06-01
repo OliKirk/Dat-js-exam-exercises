d71771866a8dd4efa9ef5ce1ce0ca39dbee8a516
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-06-01 13:50:09 +0200

    1

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -5,29 +5,29 @@ window.addEventListener("load", initApp);
 let usersGlobal = [];
 
 async function initApp() {
-    usersGlobal = await getUsers();
-    displayUsers(usersGlobal);
+  usersGlobal = await getUsers();
+  displayUsers(usersGlobal);
 
-    // displayAdmins(users);
+  //   displayAdmins(users);
 }
 
 async function getUsers() {
-    const response = await fetch("users.json");
-    const data = await response.json();
-    return data;
+  const response = await fetch("users.json");
+  const data = await response.json();
+  return data;
 }
 
 function displayUsers(listOfUsers) {
-    for (const user of listOfUsers) {
-        const html = /*html*/ `
+  for (const user of listOfUsers) {
+    const html = /*html*/ `
             <li>${user.name} (${user.role}), active: ${user.active}</li>
         `;
-        console.log(html);
-        document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
-    }
+    console.log(html);
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+  }
 }
 
 function displayAdmins() {
-    const admins = usersGlobal.filter(user => user.role === "admin");
-    displayUsers(admins);
+  const admins = usersGlobal.filter((user) => user.role === "admin");
+  displayUsers(admins);
 }
