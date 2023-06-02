c3b24cd9d9899934e6efbc498f02ed0b3ed88c95
OliKirk <owk0907@gmail.com>
2023-06-03 00:19:11 +0200

    1

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,29 +1,44 @@
+// evxercise 07
 "use strict";
 
-const product = {
-  name: "kasse",
-  price: 30,
-  inStock: true,
-
-  name: "box",
-  price: 50,
-  inStock: true,
-
-  name: "bord",
-  price: 100,
-  inStock: false,
-};
+const products = [
+  {
+    name: "kasse",
+    price: 30,
+    inStock: true,
+  },
+  { name: "box", price: 50, inStock: true },
+  { name: "bord", price: 100, inStock: false },
+];
 
 window.addEventListener("load", initApp);
 
 function initApp() {
-  showProducts();
+  console.log(products);
+  showProducts(products);
+  //   document.querySelector("#select-sort-by").addEventListener("change", sortByProducts);
 }
 
 function showProducts(listOfProducts) {
   for (const product of listOfProducts) {
+    console.log(product);
     const html = /*html*/ `
         <li> ${product.name} ${product.price} ${product.inStock} </li>`;
     document.querySelector("#list-container").insertAdjacentHTML("beforeend", html);
   }
 }
+
+// ===================== Virker Ikke =========================
+// function sortByProducts() {
+//   const sortValue = document.querySelector("#select-sory-by").value;
+//   const products = showProducts();
+//   let results = [];
+//   if (sortValue === price) {
+//     results = products.sort(products);
+//   } else if (sortValue === inStock) {
+//     results = products.sort();
+//   } else {
+//     results = products.sort((product) => product[sortValue] === "true");
+//   }
+//   showProducts(results);
+// }
--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,17 @@
-"use strict";
+// exercise 10
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  console.log(teachers);
+  showTeachers(teachers);
+}
+
+function showTeachers(listOfTeachers) {
+  for (const teacher of listOfTeachers) {
+    const html = /*html*/ ` 
+        <li> ${teacher.name} ${teacher.email}</li>`;
+    document.querySelector("#teachers-list").insertAdjacentHTML("beforeend", html);
+  }
+}
