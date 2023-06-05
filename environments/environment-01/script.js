7389277b5a50dd0f69bc9391a5fa2e73aa4fda37
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-06-05 14:50:51 +0200

    It Works

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,33 +1,67 @@
-"use strict";
+// ==================================== exersice 01 ===============================================
+// "use strict";
 
-window.addEventListener("load", initApp);
+// window.addEventListener("load", initApp);
 
-let usersGlobal = [];
+// let usersGlobal = [];
 
-async function initApp() {
-  usersGlobal = await getUsers();
-  displayUsers(usersGlobal);
+// async function initApp() {
+//   usersGlobal = await getUsers();
+//   displayUsers(usersGlobal);
+// }
 
-  //   displayAdmins(users);
-}
+// async function getUsers() {
+//   const response = await fetch("users.json");
+//   const data = await response.json();
+//   return data;
+// }
 
-async function getUsers() {
-  const response = await fetch("users.json");
-  const data = await response.json();
-  return data;
-}
+// function displayUsers(listOfUsers) {
+//   for (const user of listOfUsers) {
+//     if (user.role === "admin") {
+//       const html = /*html*/ `
+//             <li>${user.name} (${user.role}), active: ${user.active}</li>
+//         `;
+//       console.log(html);
+//       document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+//     }
+//   }
+// }
 
-function displayUsers(listOfUsers) {
-  for (const user of listOfUsers) {
-    const html = /*html*/ `
-            <li>${user.name} (${user.role}), active: ${user.active}</li>
-        `;
-    console.log(html);
-    document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
-  }
-}
+// ==================================== exersice 02 ===============================================
 
-function displayAdmins() {
-  const admins = usersGlobal.filter((user) => user.role === "admin");
-  displayUsers(admins);
-}
+// "use strict";
+
+// let globalUsers = [];
+
+// window.addEventListener("load", initApp);
+
+// async function initApp() {
+//   console.log("Vi virker");
+//   globalUsers = await getUsers();
+//   displayUsers(globalUsers);
+//   displayRoleCouint();
+// }
+
+// async function getUsers() {
+//   const reponse = await fetch("users.json");
+//   const data = reponse.json();
+//   return data;
+// }
+
+// function displayUsers(listOfUsers) {
+//   for (const user of listOfUsers) {
+//     const html = /*html*/ `
+//     <li>${user.name} ${user.role} ${user.active} </li>`;
+//     document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// function displayRoleCouint(users) {
+//   const admin = globalUsers.filter((user) => user.role === "admin");
+//   document.querySelector("#admin-count").textContent = admin.length;
+//   const user = globalUsers.filter((user) => user.role === "user");
+//   document.querySelector("#user-count").textContent = user.length;
+//   const guest = globalUsers.filter((user) => user.role === "guest");
+//   document.querySelector("#guest-count").textContent = guest.length;
+// }
