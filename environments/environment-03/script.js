adf933c099831cf2bbad797b538c06102c1246e2
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-06-07 11:13:59 +0200

    6-7-8-9 done!

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,153 @@
-"use strict";
+// ==================================== exersice 06 ===============================================
+
+// "use strict";
+
+// window.addEventListener("load", initApp);
+
+// function initApp() {
+//   showProducts(products);
+//   document.querySelector("#create-form").addEventListener("submit", createProduct);
+// }
+
+// const products = [
+//   {
+//     name: "computer",
+//     price: 10000,
+//     inStock: true,
+//   },
+//   {
+//     name: "playstation 5",
+//     price: 5000,
+//     inStock: false,
+//   },
+//   {
+//     name: "xbox serios x",
+//     price: 4500,
+//     inStock: true,
+//   },
+// ];
+
+// function showProducts(products) {
+//   document.querySelector("#list-container").innerHTML = "";
+//   for (const product of products) {
+//     if (product.inStock === true) {
+//       const html = /*html*/ `
+//         <li>${product.name} ${product.price} ${product.inStock}</li>`;
+//       document.querySelector("#list-container").insertAdjacentHTML("beforeend", html);
+//     }
+//   }
+// }
+
+// function createProduct(event) {
+//   event.preventDefault();
+
+//   const name = document.querySelector("#product-name").value;
+//   const price = Number(document.querySelector("#product-price").value);
+//   const inStock = document.querySelector("#stock").checked;
+
+//   const newProduct = { name: name, price: price, inStock: inStock };
+//   products.push(newProduct);
+//   showProducts(products);
+// }
+
+// ==================================== exersice 07 ===============================================
+
+// "use strict";
+
+// window.addEventListener("load", initApp);
+
+// const products = [
+//   {
+//     name: "computer",
+//     price: 10000,
+//     inStock: true,
+//   },
+//   {
+//     name: "playstation 5",
+//     price: 5000,
+//     inStock: false,
+//   },
+//   {
+//     name: "xbox serios x",
+//     price: 4500,
+//     inStock: true,
+//   },
+// ];
+
+// function initApp() {
+//   showProducts(products);
+//   document.querySelector("#create-form").addEventListener("submit", createProduct);
+// }
+
+// function showProducts(products) {
+//   document.querySelector("#list-container").innerHTML = "";
+//   products.sort((a, b) => b.inStock - a.inStock);
+//   for (const product of products) {
+//     const html = /*html*/ `
+//     <li>${product.name} ${product.price} ${product.inStock}</li>`;
+//     document.querySelector("#list-container").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// function createProduct(event) {
+//   event.preventDefault();
+
+//   const name = document.querySelector("#product-name").value;
+//   const price = document.querySelector("#product-price").value;
+//   const inStock = document.querySelector("#stock").checked;
+
+//   const newProduct = { name: name, price: price, inStock: inStock };
+//   products.push(newProduct);
+//   showProducts(products);
+// }
+
+// ==================================== exersice 08 ===============================================
+
+// "use strict";
+
+// window.addEventListener("load", initApp);
+
+// const products = [
+//   {
+//     name: "computer",
+//     price: 10000,
+//     inStock: true,
+//   },
+//   {
+//     name: "playstation 5",
+//     price: 5000,
+//     inStock: false,
+//   },
+//   {
+//     name: "xbox serios x",
+//     price: 4500,
+//     inStock: true,
+//   },
+// ];
+
+// function initApp() {
+//   showProducts(products);
+//   document.querySelector("#select-sort-by").addEventListener("change", sortByProducts);
+// }
+
+// function showProducts(products) {
+//   document.querySelector("#list-container").innerHTML = "";
+//   for (const product of products) {
+//     const html = /*html*/ `
+//         <li>${product.name} ${product.price} ${product.inStock}</li>`;
+//     document.querySelector("#list-container").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// function sortByProducts() {
+//   const sortBy = document.querySelector("#select-sort-by").value;
+
+//   if (sortBy === "name") {
+//     products.sort((a, b) => a.name.localeCompare(b.name));
+//   } else if (sortBy === "price") {
+//     products.sort((a, b) => a.price - b.price);
+//   } else if (sortBy === "inStock") {
+//     products.sort((a, b) => a.inStock - b.inStock);
+//   }
+//   showProducts(products);
+// }
--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,29 @@
-"use strict";
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  showTeachers(teachers);
+  sortByName(teachers);
+  sortByEmail(teachers);
+}
+
+function showTeachers(teachers) {
+  document.querySelector("#teachers-list").innerHTML = "";
+  for (const teacher of teachers) {
+    const html = /*html*/ `
+    <li>${teacher.name} ${teacher.email}</li>`;
+    document.querySelector("#teachers-list").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+// sort funktion virker kun hvert for sig. så husk at udkomenter for at se om de virker
+function sortByName() {
+  teachers.sort((a, b) => a.name.localeCompare(b.name));
+  showTeachers(teachers);
+}
+
+function sortByEmail() {
+  teachers.sort((a, b) => a.email.localeCompare(b.email));
+  showTeachers(teachers);
+}
